3d18b8babf046888d38112fc53087d080b75c19f
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-03-26 10:42:54 +0100

    Implemented WordVectors dump and tests

--- src/dump.jl
+++ src/dump.jl
@@ -11,14 +11,12 @@ Writes embeddings to disk.
   * `kind::Symbol` specifies whether the embeddings file is textual (`:text`)
 or binary (`:binary`); default `:binary`
 """
-function dump(file::IO,
-              wv::WordVectors{S,T,H};
-              kind::Symbol=:binary
+function dump(fid::IO, wv::WordVectors{S,T,H}; kind::Symbol=:binary
              ) where {S<:AbstractString, T<:Real, H<:Integer}
     if kind == :binary
-        #TODO
+        _dump_binary(fid, wv)
     elseif kind == :text
-        #TODO
+        _dump_text(fid, wv)
     else
         throw(ErrorException("Supported values for the kind keyword"*
                              "argument are :text and :binary."))
@@ -26,11 +24,31 @@ function dump(file::IO,
 
 end
 
-function dump(file::AbstractString,
-              wv::WordVectors{S,T,H};
-              kind::Symbol=:binary
+function dump(file::AbstractString, wv::WordVectors{S,T,H}; kind::Symbol=:binary
              ) where {S<:AbstractString, T<:Real, H<:Integer}
     open(file, "w") do fid
         dump(fid, wv, kind=kind)
+    end
+end
+
+function _dump_binary(fid::IO, wv::WordVectors{S,T,H}
+                     ) where {S<:AbstractString, T<:Real, H<:Integer}
+    vector_size, vocab_size = size(wv.vectors)
+    println(fid, "$vocab_size $vector_size")
+    for i in 1:vocab_size
+        write(fid, wv.vocab[i])
+        write(fid, ' ')
+        write(fid, Float32.(wv.vectors[:,i]))
+        write(fid, '\n')
+    end
+end
+
+function _dump_text(fid::IO, wv::WordVectors{S,T,H}
+                   ) where {S<:AbstractString, T<:Real, H<:Integer}
+    vector_size, vocab_size = size(wv.vectors)
+    println(fid, "$vocab_size $vector_size")
+    for i in 1:vocab_size
+        nstr = join(map(string, wv.vectors[:,i]), " ")
+        println(fid, "$(wv.vocab[i]) $(nstr)")
     end
 end
--- test/dump.jl
+++ test/dump.jl
@@ -1,6 +1,6 @@
 function fake_wordvectors(type_element::Type{T}=Float32) where T<:AbstractFloat
     vocab = ["a", "b", "c", "d", "e"]
-    vectors = diagm(0=>[1.0,2,4,5,12])
+    vectors = diagm(0 => T[1.0,2,4,5,12])
     vocab_hash = Dict(v => i for (i,v) in enumerate(vocab))
     return WordVectors(vocab, vectors, vocab_hash)
 end
@@ -8,21 +8,18 @@ end
 @testset "Dump embeddings" begin
     filepath, io = mktemp()
     for T in [Float32, Float64]
-        for kind in [:binary]
+        for kind in [:text, :binary]
             # create data
             wv_orig = fake_wordvectors(T)
             # dump data
-            try
-                dump(filepath, wv, kind=kind)
-                # reload data
-                wv_loaded = wordvectors(filepath, T, kind=kind)
-                # tests
-                @test wv_orig.vectors == wv_loaded.vectors
-                @test wv_orig.vocab == wv_loaded.vocab
-                @test wv_orig.vocab_hash == wv_loaded.vocab_hash
-            catch
-                @test_broken false
-            end
+            dump(filepath, wv_orig, kind=kind)
+            # reload data
+            wv_loaded = wordvectors(filepath, T, kind=kind, normalize=false)
+            # tests
+            @test eltype(wv_orig.vectors) == eltype(wv_loaded.vectors)
+            @test wv_orig.vectors == wv_loaded.vectors
+            @test wv_orig.vocab == wv_loaded.vocab
+            @test wv_orig.vocab_hash == wv_loaded.vocab_hash
         end
     end
 end
