28c888fc112d7b3dc4ea24608b13a2503934562b
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-03-31 22:13:25 +0200

    Added documentation, removed default language symbol value

--- src/conceptnet2wv.jl
+++ src/conceptnet2wv.jl
@@ -1,8 +1,14 @@
-# Convert a ConceptNet object to a WordVectors object
+"""
+    conceptnet2wv(cptnet, language)
+
+Converts a `ConceptNet` object, `cptnet` to a `WordVectors` object.
+The `language` of the word embeddings has to be specified explicitly
+as a `Symbol` or `Languages.Language` (Conceptnet embeddings can be
+multilingual).
+"""
 function conceptnet2wv(cptnet::ConceptNet{L,K,E},
                        language::Languages.Language;
                       ) where {L<:Language, K<:AbstractString, E<:AbstractFloat}
-
     embeddings = cptnet.embeddings[language]
     vector_size = cptnet.width
     vocab_size = length(embeddings)
@@ -16,7 +22,7 @@ function conceptnet2wv(cptnet::ConceptNet{L,K,E},
 end
 
 function conceptnet2wv(cptnet::ConceptNet{L,K,E},
-                       language::Symbol=:none;
+                       language::Symbol;
                       ) where {L<:Language, K<:AbstractString, E<:AbstractFloat}
     conceptnet2wv(cptnet, ConceptnetNumberbatch.LANGUAGES[language])
 end
