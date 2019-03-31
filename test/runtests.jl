26d5f25d83ecaca28d0b9ae77a67c79f7ed95ab3
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-03-31 22:13:57 +0200

    Added ConceptNet 2 WordVectors tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,10 @@ using Test
 using LinearAlgebra
 using EmbeddingsAnalysis
 using Word2Vec
+using ConceptnetNumberbatch
 
 include("dump.jl")
+include("conceptnet2wv.jl")
 include("similarity_order.jl")
 
 end
