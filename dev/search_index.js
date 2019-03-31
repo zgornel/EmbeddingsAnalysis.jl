9b2907caabb8f6280ae3dde6675ee0453353ac02
autodocs <autodocs>
2019-03-31 20:34:03 +0000

    build based on 26d5f25

--- dev/search_index.js
+++ dev/search_index.js
@@ -33,11 +33,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "api/#Base.dump-Union{Tuple{H}, Tuple{T}, Tuple{S}, Tuple{IO,WordVectors{S,T,H}}} where H<:Integer where T<:Real where S<:AbstractString",
+    "location": "api/#EmbeddingsAnalysis.conceptnet2wv-Union{Tuple{E}, Tuple{K}, Tuple{L}, Tuple{ConceptNet{L,K,E},Language}} where E<:AbstractFloat where K<:AbstractString where L<:Languages.Language",
     "page": "API Reference",
-    "title": "Base.dump",
+    "title": "EmbeddingsAnalysis.conceptnet2wv",
     "category": "method",
-    "text": "dump(wv::WordVectors, filename [;kind=:binary])\n\nWrites embeddings to disk.\n\nArguments\n\nwv::WordVectors the embeddings\nfilename::AbstractString the embeddings file name\n\nKeyword arguments\n\nkind::Symbol specifies whether the embeddings file is textual (:text)\n\nor binary (:binary); default :binary\n\n\n\n\n\n"
+    "text": "conceptnet2wv(cptnet, language)\n\nConverts a ConceptNet object, cptnet to a WordVectors object. The language of the word embeddings has to be specified explicitly as a Symbol or Languages.Language (Conceptnet embeddings can be multilingual).\n\n\n\n\n\n"
 },
 
 {
@@ -46,6 +46,14 @@ var documenterSearchIndex = {"docs": [
     "title": "EmbeddingsAnalysis.similarity_order",
     "category": "method",
     "text": "similarity_order(wv::WordVectors, alpha=-0.65)\n\nPost-processes the word embeddings wv so that the embeddings capture more information than directly apparent through a linear transformation that adjusts the similarity order of the model. The function returns a new WordVectors object containing the processed embeddings.\n\nReferences:\n\nArtetxe et al. \"Uncovering divergent linguistic information in  word embeddings with lessons for intrinsic and extrinsic evaluation\",  2018\n\n\n\n\n\n"
+},
+
+{
+    "location": "api/#Base.dump-Union{Tuple{H}, Tuple{T}, Tuple{S}, Tuple{IO,WordVectors{S,T,H}}} where H<:Integer where T<:Real where S<:AbstractString",
+    "page": "API Reference",
+    "title": "Base.dump",
+    "category": "method",
+    "text": "dump(wv::WordVectors, filename [;kind=:binary])\n\nWrites embeddings to disk.\n\nArguments\n\nwv::WordVectors the embeddings\nfilename::AbstractString the embeddings file name\n\nKeyword arguments\n\nkind::Symbol specifies whether the embeddings file is textual (:text)\n\nor binary (:binary); default :binary\n\n\n\n\n\n"
 },
 
 {
