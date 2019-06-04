c9c9be055cb866704d0abd0505190a62b2dea6d2
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-06-04 10:20:51 +0200

    Minor refactor+documentation of similarity_order

--- src/similarity_order.jl
+++ src/similarity_order.jl
@@ -6,6 +6,10 @@ information than directly apparent through a linear transformation that
 adjusts the similarity order of the model. The function returns a new
 `WordVectors` object containing the processed embeddings.
 
+# Arguments
+  * `wv::WordVectors` the word embeddings
+  # `alpha::AbstractFloat` the `α` parameter of the algorithm (default -0.65)
+
 # References:
   * [Artetxe et al. \"Uncovering divergent linguistic information in
      word embeddings with lessons for intrinsic and extrinsic evaluation\",
@@ -13,11 +17,11 @@ adjusts the similarity order of the model. The function returns a new
 """
 function similarity_order(wv::WordVectors{S,T,H},
                           alpha::T=T(-0.65)
-	   ) where {S<:AbstractString, T<:Real, H<:Integer}
-   n = length(wv.vocab)
-   X = wv.vectors
-   M = X*X'
-   L, Q = eigen(M)
-   Wₐ= Q .* (L.^alpha)
-   return WordVectors{S,T,H}(wv.vocab, Wₐ*X, wv.vocab_hash)
+                         ) where {S<:AbstractString, T<:Real, H<:Integer}
+    n = length(wv.vocab)
+    X = wv.vectors
+    M = X*X'
+    L, Q = eigen(M)
+    Wₐ= Q .* (L.^alpha)
+    return WordVectors{S,T,H}(wv.vocab, Wₐ*X, wv.vocab_hash)
 end
