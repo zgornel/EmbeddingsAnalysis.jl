0bfd5096da56725cdb9ea52e2db9201b8ff7bd7d
Corneliu Cofaru <cornel@oxoaresearch.com>
2019-11-30 16:59:48 +0100

    Minor changes in translator

--- src/translator.jl
+++ src/translator.jl
@@ -1,24 +1,28 @@
+#TODO(Corneliu):
+# - Define necessary methods i.e. supported signatures
+# - Support ngram_complexity
+# - Performance tweaks (if necessary)
 function translate(imodel,
                    omodel,
                    word;
                    n=10,
                    threshold=0.5,
-                   vocab=omodel.vocab,
-                   ofilter=identity)
+                   vocab=nothing,
+                   ofilter=x->true)
 
 	if !in(word, imodel.vocab)
         return [word]
     end
 	pos, scores = cosine_vec(omodel, get_vector(imodel, word), n, vocab=vocab)
     pos = pos[scores .>= threshold]
-    try
+    if isempty(pos)
+        return [word]
+    else
         candidates = filter(ofilter, omodel.vocab[pos])
         if isempty(candidates)
             return [word]
         else
             return candidates
         end
-    catch
-        return [word]
     end
 end
